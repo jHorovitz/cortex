eafb8df7a089c538c061638e29650e32c3dc9504
Omer Spillinger <ospillinger@users.noreply.github.com>
2019-01-29 15:25:11 -0800

    Update CORTEX_VERSION variables
    (cherry picked from commit e93c5fc271dd5dbec570ed2b77ed2eb2a7c2ae16)

--- build/images.sh
+++ build/images.sh
@@ -4,16 +4,10 @@ set -euo pipefail
 DIR=$1
 IMAGE=$2
 CORTEX_VERSION=0.1.0
-CORTEX_VERSION_MINOR=0.1
-CORTEX_VERSION_MAJOR=0
 
 docker build . -f $DIR/Dockerfile -t cortexlabs/$IMAGE \
-                                  -t cortexlabs/$IMAGE:$CORTEX_VERSION \
-                                  -t cortexlabs/$IMAGE:$CORTEX_VERSION_MINOR \
-                                  -t cortexlabs/$IMAGE:$CORTEX_VERSION_MAJOR
+                                  -t cortexlabs/$IMAGE:$CORTEX_VERSION
 
 echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
 
 docker push cortexlabs/$IMAGE:$CORTEX_VERSION
-docker push cortexlabs/$IMAGE:$CORTEX_VERSION_MINOR
-docker push cortexlabs/$IMAGE:$CORTEX_VERSION_MAJOR
